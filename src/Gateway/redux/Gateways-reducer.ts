2105170a032264faee00afa1bcfd4e18c63b5c25
KevinM226 <kmmst226@mail.rmu.edu>
2020-11-01 16:37:02 -0500

    Redux
    redux files reimplemented and Gateway names reverted

---
+++ src/Gateway/redux/Gateways-reducer.ts
@@ -0,0 +1,36 @@
+import { GatewaysState } from "./Gateways-state";
+import { GatewaysAction, GatewaysActionType, GatewaysFailedAction, GatewaysChangedAction } from "./Gateways-actions";
+
+const initialState: GatewaysState = {
+  loading: false,
+  succeed: false,
+};
+
+export function devicesReducer(
+  state: GatewaysState = initialState,
+  action: GatewaysAction
+): GatewaysState {
+  switch (action.type) {
+    case GatewaysActionType.LOADING:
+      return {
+        ...state,
+        error: undefined,
+        loading: true,
+      };
+    case GatewaysActionType.FAILED:
+      return {
+        ...state,
+        error: (action as GatewaysFailedAction).error,
+        loading: false,
+      };
+    case GatewaysActionType.CHANGE_DEVICES:
+      return {
+        ...state,
+        devices: (action as GatewaysChangedAction).devices,
+        loading: false,
+        error: undefined,
+      };
+    default:
+      return state;
+  }
+}
