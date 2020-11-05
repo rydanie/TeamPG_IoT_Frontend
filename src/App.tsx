f05b4c668ab17853d42bb93f74a6f65ab311b426
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2020-11-04 23:33:34 -0500

    Fromating of Devices complete
    It looks correct (completely different funcionly, all buttons submit, all text boxes are for name).

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ function App() {
             <Route path="/devices">
               <Devices />
             </Route>
-
           </Switch>
         </div>
       </Router>
--- src/devices/DevicesForm.tsx
+++ src/devices/DevicesForm.tsx
@@ -27,6 +27,8 @@ export default function DevicesForm({ loading, onCreateDevice }: DevicesFormProp
         onCreateDevice(data.name);
     };
 
+    /*name is the devices name, id is mac address, required means it must be entered
+    before it lets you move on*/
     return <Card className="col-lg-6">
         <CardHeader>
             <h3 className="mb-0">DEVICE ENROLLEMENT</h3>
@@ -34,13 +36,13 @@ export default function DevicesForm({ loading, onCreateDevice }: DevicesFormProp
         <CardBody>
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <FormGroup>
-                    <Label for="device-name">Gateway Name</Label>
+                    <Label for="device-name">Device Name:</Label>
                     <Controller
                         as={Input}
                         name="name"
                         control={control}
                         defaultValue=""
-                        placeholder="Gateway Name"
+                        placeholder="Device Name"
                         id="device-name"
                         rules={{ required: true }}
                     />
@@ -48,10 +50,35 @@ export default function DevicesForm({ loading, onCreateDevice }: DevicesFormProp
                         <div className="alert alert-danger" role="alert">
                             <strong>Device name</strong> is required
                                 </div>}
-                    <Label for="device-name">Device Name</Label>
-
+                    <Label for="device-name">Mac Address:</Label>
+                    <Controller
+                        as={Input}
+                        name="name"
+                        control={control}
+                        defaultValue=""
+                        placeholder="Mac Address"
+                        id="device-name"
+                        rules={{ required: false }}
+                    />
+                    {errors.name &&
+                        <div className="alert alert-danger" role="alert">
+                            <strong>Device name</strong> is required
+                                </div>}
+                    <Label for="device-name">Gateway Connected ti:</Label>
+                        <Controller
+                            as={Input}
+                            name="name"
+                            control={control}
+                            defaultValue=""
+                            placeholder="Gateway Name"
+                            id="device-name"
+                            rules={{ required: false }}
+                    />
+                    {errors.name &&
+                        <div className="alert alert-danger" role="alert">
+                            <strong>Device name</strong> is required
+                                </div>}
                 </FormGroup>
-
                 <Button
                     type="submit"
                     color="success"
