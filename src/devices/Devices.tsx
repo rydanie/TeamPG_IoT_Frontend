851e369e5549736566c69cef9a4ad456578cbd87
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2020-11-07 13:07:05 -0500

    Freed Text Inputs
    Can still submit name, but does not save other fields

--- src/devices/Devices.tsx
+++ src/devices/Devices.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
+import {Button, Form, FormGroup, Input, Label} from "reactstrap";
+import deviceService from "./services/devices-service";
 import { connect, ConnectedProps } from "react-redux";
 
 import { RootState } from "redux/root-reducer";
@@ -60,5 +62,47 @@ function Devices({
     );
 }
 
-
 export default connector(Devices);
+
+/*
+export default function Hello(){
+
+    const [name, setName] = useState("Test");
+    const [updatedName, setUpdatedName] = useState("");
+
+    useEffect(() =>{
+        /*fetch("http://localhost:8080/devices/get-name")
+            .then(response => response.json())
+            .then(data =>{
+                setName(data);
+            });*//*
+        const result = deviceService.getName();
+        result.then(theName => {
+            setName(theName);
+        })
+
+    },[]);
+
+    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        let newValue = event.target.value;
+        setUpdatedName(newValue);
+    }
+
+    const handleNameSubmit = () => {
+        const result = deviceService.changeName(updatedName);
+        result.then(newName => {
+            setName(newName);
+        })
+    }
+
+    return <div>
+        Hello {name}<br/>
+        <Form>
+            <FormGroup>
+                <Label for="name">Name</Label>
+                <Input type="text" name="name" id="name" onChange={handleNameChange} value={updatedName} placeholder="Please enter your name" />
+            </FormGroup>
+            <Button onClick={handleNameSubmit} color="danger">Submit</Button>{' '}
+        </Form>
+    </div>;
+}*/
