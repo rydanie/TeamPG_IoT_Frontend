cdc7d5c612e5c7974a689e6bed88372bef3bf1fa
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2020-11-08 23:00:42 -0500

    Revert "Revert "Don't touch devices, fix gateways""
    This reverts commit 271d8fba01016a3ba5dd05b1ced528fc05f0f8fe.

--- src/devices/services/devices-service.ts
+++ src/devices/services/devices-service.ts
@@ -10,7 +10,7 @@ class DevicesService {
     }
 
     async create(name: string, macAdd: string, conName: string): Promise<Device[]> {
-        /*
+
         await this.http.post(
             "/",
             { name, macAdd, conName},
@@ -20,13 +20,13 @@ class DevicesService {
                     Accept: "application/json",
                 },
             }
-        );*/
+        );
 
-        axios.post("http://localhost:3000/device",{
+        /*axios.post("http://localhost:3000/device",{
             "name": name,
             "macAdd": macAdd,
             "conName": conName,
-        });
+        });*/
 
         return await this.devices();
     }
