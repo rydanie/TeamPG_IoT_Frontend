0fa84dcd656d6e9baee9be17c7222bc513cdd783
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2020-12-13 18:55:49 -0500

    Revert "Most Important Part of the Project"
    This reverts commit d64f2e528b7ad0c7fe165b4d7ca2bd0bd90f2161.

--- src/help/Help.tsx
+++ src/help/Help.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Button } from "reactstrap";
+import { Table } from "reactstrap";
 import Dropdown from "../bootstrap/js/bootstrap.js"
 
 import "../App.css";
@@ -36,9 +36,5 @@ export default function Help(){
                  </tr>
              </tbody>
          </Table>
-         <Button outline color="success" onClick={ (e) =>
-           console.log(window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
-           ) }>If you have more questions click here</Button>
     </div>
 }
